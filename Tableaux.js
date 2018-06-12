9ec6233259a86307c700bbacafb9446e8cc6098c
Adeline <adeline.contact@gmail.com>
2018-06-12 12:00:23 +0200

    exercices 1 à 7 array

--- Tableaux.js
+++ Tableaux.js
@@ -1,8 +1,9 @@
 var mois = ['janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre'];
 
-document.write(mois[2]+"<br />"+"<br />");
-document.write(mois[5]+"<br />"+"<br />");
+document.write(mois[2]+"<br />"+"<br />"); //Affiche la 3ème valeur du tableau "mois"
+document.write(mois[5]+"<br />"+"<br />"); //Affiche la valeur du tableau "mois" dont l'index est 5
 document.write(mois[10]+"<br />"+"<br />");
 
-mois [7] = "août";
-document.write(mois.join()+"<br />"+"<br />");
+mois [7] = "août"; //Affecte une nouvelle valeur à l'index 7
+document.write(mois.join()+"<br />"+"<br />"); //Affiche toutes les valeurs du tableau "mois"
+
