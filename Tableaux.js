91bcc1158b95877abf03913ccfa7b86d46c4df17
Adeline <adeline.contact@gmail.com>
2018-06-12 11:47:55 +0200

    ajout sources locales

---
+++ Tableaux.js
@@ -0,0 +1,8 @@
+var mois = ['janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre'];
+
+document.write(mois[2]+"<br />"+"<br />");
+document.write(mois[5]+"<br />"+"<br />");
+document.write(mois[10]+"<br />"+"<br />");
+
+mois [7] = "août";
+document.write(mois.join()+"<br />"+"<br />");
