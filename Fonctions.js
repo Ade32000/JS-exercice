423f537885a9f9ef0798a62ee72f28b15d1eeeb2
Adeline <adeline.contact@gmail.com>
2018-06-12 15:38:16 +0200

    ex 1 et 2 js Fonctions

--- Fonctions.js
+++ Fonctions.js
@@ -1,17 +1,31 @@
 var a = 5;
 var b = 2;
 
-function vrai(c,d) {
-	if (c > d){
+function vrai(c,d) 
+{
+	if (c > d)
+	{
 		document.write("La vérité sort toujours de la bouche d'une fonction...ou pas!");
 		return true;		
 	}
-	else{
-		document.write("Non, non, non, c'est faux");
+	else
+	{
+		document.write("Non, non, non, c'est faux !!!");
 		return false;		
 	}
 }
 
 vrai(a,b);
 
+document.write("<br /><br />");
 
+function caractere() 
+{
+	var char = "J'ai bon caractère";
+	return char;
+	
+}
+
+document.write(caractere());
+
+document.write("<br /><br />");
