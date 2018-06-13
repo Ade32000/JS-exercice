7d52e5998573c1d3237bf913a55bc989e818d14f
Adeline <adeline.contact@gmail.com>
2018-06-13 12:08:15 +0200

    ex2 boucles js

--- Boucles.js
+++ Boucles.js
@@ -5,5 +5,16 @@ var x = 0;
 	document.write(x+'<br />');
 	}
 
+document.write("<br />"+"<br />");
 
+var nb1 = 0;
+var nb2 = 50;
 
+	while (nb1 <= 20)
+	{
+		var res = nb1 * nb2;
+		document.write(res+'<br />');
+		nb1++;
+	}
+
+document.write("<br />"+"<br />");
